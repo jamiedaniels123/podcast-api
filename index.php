8a69871b1f3b5fda05955e75916c12fdbb92a278
Ian Newton <i.newton@open.ac.uk>
2011-07-14 11:59:08 +0100

    Cake sample site removed and replaced with admin-api code

--- index.php
+++ index.php
@@ -1,55 +1,63 @@
-<?php
-/**
- * Requests collector.
- *
- *  This file collects requests if:
- *	- no mod_rewrite is avilable or .htaccess files are not supported
- *  - requires App.baseUrl to be uncommented in app/config/core.php
- *	- app/webroot is not set as a document root.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       cake
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-/**
- *  Get Cake's root directory
- */
-	define('APP_DIR', 'app');
-	define('DS', DIRECTORY_SEPARATOR);
-	define('ROOT', dirname(__FILE__));
-	define('WEBROOT_DIR', 'webroot');
-	define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
-/**
- * This only needs to be changed if the "cake" directory is located
- * outside of the distributed structure.
- * Full path to the directory containing "cake". Do not add trailing directory separator
- */
-	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH', ROOT);
+<?PHP
+/*========================================================================================*\
+	#	Coder    :  Ian Newton
+	#	Date     :  24th May,2011
+	#	Test version  
+	#	input controller to accept post requests from the admin server
+\*=========================================================================================*/
+
+require_once("./lib/config.php");
+// require_once("./lib/classes/db.class.php");
+require_once("./lib/classes/action-admin.class.php");
+require_once("./lib/classes/output.class.php");
+
+$r_data= '';
+$mysqli = new mysqli($dbLogin['dbhost'], $dbLogin['dbusername'], $dbLogin['dbuserpass'], $dbLogin['dbname']);
+
+$dataStream = file_get_contents("php://input");
+
+$dataMess=explode('=',urldecode($dataStream));
+
+if (isset($dataMess[1])) {
+
+	$data=json_decode($dataMess[1],true);
+//print_r($data);
+
+	$outObj = new Default_Model_Output_Class();
+	
+	$dataObj = new Default_Model_Action_Class($mysqli,$outObj,$apiName);	
+
+	$sqlQuery = "SELECT * FROM command_routes AS cr WHERE cr.cr_action = '".$data['command']."'";
+
+// echo 	$sqlQuery;
+	$result = $mysqli->query($sqlQuery);
+	$row = $result->fetch_object();
+	
+	if ($result->num_rows) {
+
+// Put command message on message queue and data for each request on the command queue
+
+		$m_data = $dataObj->queueAction($data['data'],$data['number'],$data['command'],$data['timestamp']);
+// Do anything now which needs to be done directly	
+
+		$cqCommand="'direct'";
+		$r_data = $dataObj->doNextAction($m_data['mqIndex'], $cqCommand);
+//		$m_data[] = array('status'=>'ACK', 'data'=>'Command sent to ! '.$row->ad_url, 'timestamp'=>time());
+
+	}else{
+		$m_data = array('status'=>'NACK', 'data'=>'Command not known on admin-api !', 'sqlQuery'=>$sqlQuery, 'timestamp'=>time());
+
 	}
 
-/**
- * Set the include path or define app and core path
- */
-	if (function_exists('ini_set')) {
-		ini_set('include_path',
-			ini_get('include_path') . PATH_SEPARATOR . CAKE_CORE_INCLUDE_PATH
-			. PATH_SEPARATOR . ROOT . DS . APP_DIR . DS
-		);
-		define('APP_PATH', null);
-		define('CORE_PATH', null);
-	} else {
-		define('APP_PATH', ROOT . DS . APP_DIR . DS);
-		define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
-	}
-	require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
+}else{
+	$m_data = array('status'=>'NACK', 'data'=>'No request values set!', 'timestamp'=>time());
+
+}
+	$sqlLogging = "INSERT INTO `api_log` (`al_message`, `al_reply`, `al_result_data`, `al_source_ip`,`al_timestamp`) VALUES ( '".$dataMess[1]."', '".serialize($m_data)."', '".serialize($r_data)."', '','".date("Y-m-d H:i:s", time())."' )";
+	$result = $mysqli->query($sqlLogging);
+// echo "mediaUrl-".$row->ad_url;
+// print_r ($m_data);
+$jsonData = json_encode($m_data);
+echo $jsonData;
+
+?>
