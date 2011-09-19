11cc19c68ba3f7bc89a48cdf1d6a721c113f1cfd
jamie <mr_t_____@hotmail.com>
2011-09-19 14:32:49 +0100

    first push to git from dev server

---
+++ lib/config.php
@@ -0,0 +1,49 @@
+<?PHP
+/*========================================================================================*\
+	#	Coder    :  Ian Newton
+	#	Date     :  25th May,2011
+	#	Admin-api config file
+/*========================================================================================*/
+
+//___Debug_________________________________________________________________________________________//
+
+//	ini_set(display_errors,On);
+	$error='';
+	$debug='';
+
+//___DB CONNECTION_________________________________________________________________________________//
+
+	$dbLogin = array ('dbhost' => "localhost", 'dbname' => "admin-api-dev", 'dbusername' => "podcastapi", 'dbuserpass' => "CZJ5SWw2TBWueBmp");
+
+//___TIME ZONE_________________________________________________________________________________//
+
+	date_default_timezone_set("Europe/London");
+
+//___API_NAME_________________________________________________________________________________//
+
+	$apiName = "admin-api";
+	$version = "dev";
+
+//____SCP/FILE/API - SOURCE/DESTINATIONS_________________________________________________________________//
+
+	$source = array( 
+		'admin-files' => '/data/web/podcast-admin-dev.open.ac.uk/www/app/webroot/upload/files/', // Set the Admin file source if needed
+		'vle-files' => '/data/web/podcast-api-dev.open.ac.uk/www/vle/upload/files/', // Set the VLE file source if needed for testing
+		'vle-app' => 'http://podcast-api-dev.open.ac.uk/vle/', // Set for testing only this is the VLE URL (api) which we poll for commands
+		'admin-app' => 'http://podcast-admin-dev.open.ac.uk/', // This is for completeness so we know where messages come from
+	  	'media-scp' => 'media-transfer-dev@media-podcast-api-dev.open.ac.uk:/data/web/media-podcast-api-dev.open.ac.uk/file-transfer/source/',
+  		'encoder-scp' => '/Volumes/Data/Episode/EpisodeEngine/Outputs/'
+	);
+  
+	$destination = array( 
+		'media-scp' => 'media-transfer-dev@media-podcast-api-dev.open.ac.uk:/data/web/media-podcast-api-dev.open.ac.uk/file-transfer/destination/', 
+  		'encoder-scp' => '/Volumes/Data/Episode/EpisodeEngine/Inputs/',
+		'admin-api' => 'http://podcast-api-dev.open.ac.uk', 
+		'media-api' => 'http://media-podcast-api-dev.open.ac.uk', 
+		'encoder-api' => 'http://kmi-encoder-api-dev.open.ac.uk', 
+		'admin-vle' => 'http://podcast-admin-dev.open.ac.uk/vles/add/', 
+		'admin-app' => 'http://podcast-admin-dev.open.ac.uk/callbacks/add/',
+		'debug_email' => 'jdd7@openmail.ac.uk'		
+	);
+
+?>
---
+++ poll-vle.php
@@ -0,0 +1,37 @@
+<?PHP
+/*========================================================================================*\
+	#	Coder    :  Ian Newton
+	#	Date     :  24th May,2011
+	#	Test version  
+	#	controller to process actions queued in the media_actions table and report status to the admin server
+\*=========================================================================================*/
+
+require_once("lib/config.php");
+require_once("lib/classes/action-admin.class.php");
+require_once("lib/classes/output.class.php");
+
+// Initialise objects
+	$mysqli = new mysqli($dbLogin['dbhost'], $dbLogin['dbusername'], $dbLogin['dbuserpass'], $dbLogin['dbname']);
+	$outObj = new Default_Model_Output_Class($mysqli);
+	$dataObj = new Default_Model_Action_Class($mysqli,$outObj);	
+
+if (isset($_REQUEST['time']) && $_REQUEST['time']>=1) $time = $_REQUEST['time']; else $time=1;
+if (isset($_REQUEST['number']) && $_REQUEST['number']>=1) $number = $_REQUEST['number']; else $number=1;
+
+// Poll for any completed or failed commands on Media and Encoder queues
+
+	for ( $i = 1; $i <= $number; $i++) {
+
+//		$dataObj->pollMedia();
+
+//		$dataObj->pollEncoder();
+
+		$dataObj->pollVLE($_REQUEST['request']);
+
+// sleep for n seconds
+flush();
+//		echo $i." - ";
+		sleep($_REQUEST['time']);
+	}
+
+?>
